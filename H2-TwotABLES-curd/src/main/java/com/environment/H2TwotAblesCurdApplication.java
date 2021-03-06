0654ac48c58c4304785b953480477badf092b8cc
rohith <rohith@DESKTOP-1SO89Q0>
2021-03-06 20:34:27 +0530

    This is my second Project commit H2-TwotABLES-curd project added into repository

---
+++ H2-TwotABLES-curd/src/main/java/com/environment/H2TwotAblesCurdApplication.java
@@ -0,0 +1,14 @@
+
+package com.environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2TwotAblesCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2TwotAblesCurdApplication.class, args);
+	}
+
+}
---
+++ H2-TwotABLES-curd/src/test/java/com/environment/H2TwotAblesCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2TwotAblesCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
